829995ca11523600e095c6b9cd5e8863298c615b
clach04 <clach04@gmail.com>
2026-06-28 14:37:17 -0700

    Initial library and demo - incomplete
    Partially mirrors https://github.com/clach04/py-posix_tz in approach,
    with some differences in model/structure. Initial target is Pebble SDK
    use case, specifically https://github.com/clach04/pebble_tz/tree/clay_choice
    where minute offsets are currently used. Other difference is dst boolean
    versus dst offset.

--- demo.c
+++ demo.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <time.h>
 
+#include "microtz.h"
+
 int main (int argc, char *argv[])
 {
     struct tm *local_tm=NULL;
@@ -13,23 +15,19 @@ int main (int argc, char *argv[])
     static char buffer[]="00:00";
     static char time_str[]="timezonename 00:00";  // need one string per layer, reusing same buffer results in same text
 
-
-//#define LOCAL_OFFSET_IN_MINS 0  // UTC / GMT0
-//#define TZ_NAME "UTC"
-
-//#define LOCAL_OFFSET_IN_MINS (-8 * 60)  // Los Angeles Winter time
-//#define TZ_NAME "PST"
-
-#define LOCAL_OFFSET_IN_MINS (-7 * 60)  // Los Angeles DST
-#define TZ_NAME "PDT"
-
+    //microtz_info tz = microtz_parse("UTC");
+    //microtz_info tz = microtz_parse("PST8PDT,M3.2.0,M11.1.0");
+    microtz_info tz = microtz_parse("PST8PDT,M3.2.0/2:00:00,M11.1.0/2:00:00");
+    //microtz_info tz = microtz_parse("IST-5:30");  // FIXME not implemented / broken
+    int local_offset_mins = microtz_offset(&tz, time(NULL));
+    const char *tz_name = (local_offset_mins != tz.offset / 60) ? tz.dst_name : tz.name;
 
     utc_time = time(NULL);
     printf("utc_time:           %d\n", utc_time);
-    converted_local_tm = utc_time + (60 * LOCAL_OFFSET_IN_MINS);
+    converted_local_tm = utc_time + (60 * local_offset_mins);
     printf("converted_local_tm: %d\n", converted_local_tm);
     local_tm = gmtime(&converted_local_tm);
     strftime(buffer, sizeof(buffer), time_format, local_tm);
-    snprintf(time_str, sizeof(time_str), "%s %s", TZ_NAME, buffer);
+    snprintf(time_str, sizeof(time_str), "%s %s", tz_name, buffer);
     printf("time: %s\n", time_str);
 }
